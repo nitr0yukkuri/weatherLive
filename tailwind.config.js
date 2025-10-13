c14792ced5f54d21616de2e08b14f23ac2caf2e7
wakato <itlogin0606@gmail.com>
2025-10-13 12:25:13 +0900

    tailwind.cssの設定を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,24 @@ const config = {
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
+    // ↓↓↓↓↓↓ ここから追加 ↓↓↓↓↓↓
+    safelist: [
+        'from-gray-800',
+        'to-black',
+        'from-pink-400',
+        'to-indigo-500',
+        'from-pink-200',
+        'to-yellow-200',
+        'to-blue-300',
+        'to-gray-300',
+        'from-pink-100',
+        'to-blue-200',
+        'to-pink-300',
+    ],
+    // ↑↑↑↑↑↑ ここまで追加 ↑↑↑↑↑↑
     theme: {
         extend: {},
     },
     plugins: [],
-}
-export default config
+};
+export default config;
